bf8142af75f0e619c1fd3d299e681779604c1e4c
Vasily <DVsXYixg>
2020-03-21 19:22:48 +0300

    feat: implement StringParser

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,45 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
+    data class StringData(
+        var indexInResult: Int = 0,
+        var string: StringBuilder = java.lang.StringBuilder()
+    )
+
+    data class BracketsData(
+        var openChar: Char,
+        var closeChar: Char,
+        var data: MutableList<StringData> = mutableListOf()
+    )
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val result = mutableListOf<String>()
+        val bracketsResult = arrayOf(
+            BracketsData('[', ']'),
+            BracketsData('<', '>'),
+            BracketsData('(', ')')
+        )
+
+        for (symbol in inputString) {
+            for (bracketsData in bracketsResult) {
+                if (symbol == bracketsData.closeChar) {
+                    if (bracketsData.data.isNotEmpty()) {
+                        val data = bracketsData.data.last();
+                        result.add(data.indexInResult, data.string.toString())
+                        bracketsData.data.removeAt(bracketsData.data.lastIndex)
+                    }
+                }
+
+                for (data in bracketsData.data) {
+                    data.string.append(symbol)
+                }
+
+                if (symbol == bracketsData.openChar) {
+                    bracketsData.data.add(StringData(result.lastIndex + 1));
+                }
+            }
+        }
+
+        return result.toTypedArray();
     }
 }
