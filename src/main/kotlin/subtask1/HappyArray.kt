68a9460d93e664be0da5bf90e21ad624cb892a00
Vasily <DVsXYixg>
2020-03-21 14:06:49 +0300

    feat: implement convertToHappy

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,32 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
+    private fun checkLast(targetArray: MutableList<Int>, next: Int): MutableList<Int> = when {
+        targetArray.size == 1 -> {
+            targetArray
+        }
+        targetArray[targetArray.lastIndex - 1] + next >= targetArray.last() -> {
+            targetArray;
+        }
+        else -> {
+            targetArray.removeAt(targetArray.lastIndex);
+            checkLast(targetArray, next);
+        }
+    }
+
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var result = emptyList<Int>().toMutableList();
+
+        for (index in sadArray.indices) {
+            if (index == 0 || index == sadArray.size - 1) {
+                result.plusAssign(sadArray[index])
+            } else if (result.last() + sadArray[index + 1] >= sadArray[index]) {
+                result.plusAssign(sadArray[index]);
+            } else {
+                result = checkLast(result, sadArray[index + 1])
+            }
+        }
+        println(result);
+        return result.toIntArray();
     }
 }
